007467f1bccbd3e69fcb27dfffc3ffd30c3636c2
Vikesz <szlankav@hotmail.com>
2025-06-08 19:56:05 +0200

    Second Commit

--- BMICalculator.py
+++ BMICalculator.py
@@ -11,11 +11,11 @@ bmi = calculate_bmi(weight, height)
 print(f"Your BMI is: {bmi:.2f}")
 
 if bmi < 18.5:
-    print("You are underweight.")
+    print("You are underweight!")
 elif 18.5 <= bmi < 25:
     print("You have a normal weight.")
 elif 25 <= bmi < 30:
-    print("You are overweight.")
+    print("You are overweight!!!")
 else:
     print("You are obese.")
     
